c7ade5419ffcc498b7ebdc46babff16e5e59ddbb
Vishal_Ingawale <vishal96ingawale@gmail.com>
2026-09-16 18:34:04 +0530

    Created SpecUtil to handle common api request config and common assertions

---
+++ src/test/java/com/api/tests/CountAPITestOptimized.java
@@ -0,0 +1,43 @@
+package com.api.tests;
+
+import com.api.constant.Role;
+import com.api.utils.AuthTokenProvider;
+import com.api.utils.ConfigManager;
+import com.api.utils.SpecUtil;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CountAPITestOptimized {
+
+    @Test
+    public void verifyCountAPIResponse(){
+        given()
+                .baseUri(ConfigManager.getProperty("BASE_URI"))
+                .and()
+                .header("Authorization", AuthTokenProvider.getToken(Role.FD))
+                .when()
+                .get("/dashboard/count")
+                .then()
+                .spec(SpecUtil.responseSpec_OK())
+                .body("message", Matchers.equalTo("Success"))
+                .time(Matchers.lessThan(1000L))
+                .body("data",Matchers.notNullValue())
+                .body("data.size()",Matchers.equalTo(3))
+                .body("data.count",Matchers.everyItem(Matchers.greaterThanOrEqualTo(0)))
+                .body("data.label",Matchers.everyItem(Matchers.not(Matchers.blankOrNullString())))
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/CountAPIResponseSchema-FD.json"));
+    }
+
+    @Test
+    public void countAPITest_MissingAuthToken(){
+        given()
+                .spec(SpecUtil.requestSpec())
+                .when()
+                .get("/dashboard/count")
+                .then()
+                .spec(SpecUtil.responseSpec_TEXT(401));
+    }
+}
