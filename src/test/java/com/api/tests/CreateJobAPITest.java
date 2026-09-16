67805e08bbba6fda9b4fa1d483f90800f264fb04
Vishal_Ingawale <vishal96ingawale@gmail.com>
2026-09-16 23:19:49 +0530

    Restore reverted files

---
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -0,0 +1,42 @@
+package com.api.tests;
+
+import com.api.constant.Role;
+import com.api.pojo.*;
+import com.api.utils.AuthTokenProvider;
+import com.api.utils.ConfigManager;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateJobAPITest {
+
+    @Test
+    public void createJobAPITest() {
+        //Creating the CreateJobPayload object
+        Customer customer = new Customer("Vishal","Ingawale","9665431543","","vishal96ingawale@gmail.com","");
+        CustomerAddress customerAddress = new CustomerAddress("F:5","Moraya Krupa","Sudarshan Nagar","Chinchwad","Pune","411033","India","Maharashtra");
+        CustomerProduct customerProduct = new CustomerProduct("2025-11-24T18:30:00.000Z","19991432448429","19991432448429","19991432448429","2025-11-24T18:30:00.000Z",1,1);
+        Problems problems = new Problems(1,"Battery Issue");
+        Problems[] problemsArray = new Problems[1];
+        problemsArray[0]=problems;
+        CreateJobPayload createJobPayload = new CreateJobPayload(0,2,1,1,customer,customerAddress,customerProduct,problemsArray);
+
+
+        given()
+                .baseUri(ConfigManager.getProperty("BASE_URI"))
+                .and()
+                .header("Authorization", AuthTokenProvider.getToken(Role.FD))
+                .contentType(ContentType.JSON)
+                .body(createJobPayload)
+                .log().all()
+                .when()
+                .post("/job/create")
+                .then()
+                .log().all()
+                .statusCode(200);
+
+
+    }
+}
